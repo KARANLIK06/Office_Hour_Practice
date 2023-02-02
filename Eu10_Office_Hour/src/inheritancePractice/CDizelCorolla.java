4fa4d8237d8b43c2d00e249999d87856ad705ef8
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-02 10:19:52 +0300

    inheritancePractice

--- Eu10_Office_Hour/src/inheritancePractice/CDizelCorolla.java
+++ Eu10_Office_Hour/src/inheritancePractice/CDizelCorolla.java
@@ -1,4 +1,19 @@
 package inheritancePractice;
 
-public class CDizelCorolla {
+public class CDizelCorolla extends BCorolla{
+    CDizelCorolla() {
+        System.out.println("Dizel corolla constructor");
+    }
+
+    public static void main(String[] args) {
+        BCorolla bCorolla = new BCorolla();
+
+        System.out.println("-------------------------");
+
+        CDizelCorolla cDizelCorolla = new CDizelCorolla();
+        System.out.println(cDizelCorolla.brand);
+        System.out.println(cDizelCorolla.productionPlace);
+        System.out.println(cDizelCorolla.model);
+
+    }
 }
